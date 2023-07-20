ae073cb35883d0612138dad084346f3d0b815518
john james <yagazhigler@gmail.com>
2023-07-20 17:34:54 +0100

    HamburgerMenu Component
    This component represents a responsive hamburger menu that can be toggled open and closed.
    It also includes a feature to close the menu when the user clicks outside the menu area.

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -1,91 +1,6 @@
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import './style.css';
-
-// const HamburgerMenu = () => {
-//   const [menuOpen, setMenuOpen] = useState(false);
-
-//   const handleMenuClick = () => {
-//     setMenuOpen(!menuOpen);
-//   };
-
-//   return (
-//     <header id="header">
-//       <div className="containerr z-50">
-//         <div id="menu-btn" className={`hamburger ${menuOpen ? 'open' : ''}`} onClick={handleMenuClick}>
-//           <span className="hamburger-top"></span>
-//           <span className="hamburger-middle"></span>
-//           <span className="hamburger-bottom"></span>
-//         </div>
-//         <div className='menu-item'>
-
-//           <ul id="menu" className={`z-50 display hamburger-ul ${menuOpen ? '' : 'hidden'} `}>
-//             <li>
-//               <Link to="/" className="link-hover font-bold link-bg justify-center fw-500 px-3 py-2 rounded">
-//                 Home
-//               </Link>
-//             </li>
-           
-//             <li>
-//               <Link to="/projects" className="font-bold link-bg link-hover justify-center px-3 py-2 rounded">
-//                 Projects
-//               </Link>
-//             </li>
-//             <li>
-//               <Link to="/skills" className="font-bold link-bg link-hover justify-center px-3 py-2 rounded">
-//                 Skills
-//               </Link>
-//             </li>
-//             <li>
-//               <Link to="/experience" className="font-bold link-bg link-hover fw-600 justify-center px-3 py-2 rounded">
-//                 Experience
-//               </Link>
-//             </li>
-//             <li>
-//               <Link to="/contact" className="font-bold link-bg link-hover justify-center px-3 py-2 rounded">
-//                 Contact
-//               </Link>
-//             </li>
-
-//             <li>
-//             <Link to="/AboutMe" className="font-bold link-bg link-hover justify-center px-3 py-2 rounded">
-//                AboutMe
-//               </Link>
-//             </li>            
-//           </ul>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// };
-
-// export default HamburgerMenu;
-
-
-
-
-/**
- * HamburgerMenu Component
- *
- * This component represents a responsive hamburger menu that can be toggled open and closed.
- * It also includes a feature to close the menu when the user clicks outside the menu area.
- */
-
-
-
-
-
-
-
-
-
-
-
-
-import React, { useState, useEffect, useRef, MouseEvent } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
-import Home from './Home';
 
 /**
  * HamburgerMenu Component
@@ -93,6 +8,7 @@ import Home from './Home';
  * This component represents a responsive hamburger menu that can be toggled open and closed.
  * It also includes a feature to close the menu when the user clicks outside the menu area.
  */
+
 const HamburgerMenu: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const menuRef = useRef<HTMLDivElement | null>(null);
@@ -101,27 +17,36 @@ const HamburgerMenu: React.FC = () => {
     setMenuOpen(!menuOpen);
   };
 
-  const handleClickOutsideMenu = (event: MouseEvent<Document>) => {
-    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+  const handleClickOutsideMenu = (event: MouseEvent) => {
+    if (
+      menuRef.current &&
+      !menuRef.current.contains(event.target as Node) &&
+      !document.getElementById('menu-btn')?.contains(event.target as Node)
+    ) {
       setMenuOpen(false);
     }
   };
 
   useEffect(() => {
-    // document.addEventListener('click', handleClickOutsideMenu);
+    document.addEventListener('mousedown', handleClickOutsideMenu);
     return () => {
-      // document.removeEventListener('click', handleClickOutsideMenu);
+      document.removeEventListener('mousedown', handleClickOutsideMenu);
     };
-  },);
+  }, []);
 
   return (
     <header id="header">
       <div className="containerr z-50">
-        <div id="menu-btn" className={`hamburger ${menuOpen ? 'open' : ''}`} onClick={handleMenuClick}>
+        <div
+          id="menu-btn"
+          className={`hamburger ${menuOpen ? 'open' : ''}`}
+          onClick={handleMenuClick}
+        >
           <span className="hamburger-top"></span>
           <span className="hamburger-middle"></span>
           <span className="hamburger-bottom"></span>
         </div>
+        
         <div className='menu-item' ref={menuRef}>
           <ul id="menu" className={`z-50 display hamburger-ul ${menuOpen ? '' : 'hidden'} `}>
             <li>
@@ -152,8 +77,8 @@ const HamburgerMenu: React.FC = () => {
             </li>
 
             <li>
-            <Link to="/AboutMe" className="font-bold link-bg link-hover justify-center px-3 py-2 rounded">
-               AboutMe
+              <Link to="/AboutMe" className="font-bold link-bg link-hover justify-center px-3 py-2 rounded">
+                AboutMe
               </Link>
             </li>            
           </ul>
@@ -164,3 +89,4 @@ const HamburgerMenu: React.FC = () => {
 };
 
 export default HamburgerMenu;
+
