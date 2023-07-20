1552f61b7d2dffb2e336cd28d8fd641e190ffc0c
john james <yagazhigler@gmail.com>
2023-07-20 14:12:34 +0100

    remove footer
    I remove the footer from all the components and have it on App.tsx that will make it display on every page of our website.

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -1,13 +1,118 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import './style.css';
+
+// const HamburgerMenu = () => {
+//   const [menuOpen, setMenuOpen] = useState(false);
+
+//   const handleMenuClick = () => {
+//     setMenuOpen(!menuOpen);
+//   };
+
+//   return (
+//     <header id="header">
+//       <div className="containerr z-50">
+//         <div id="menu-btn" className={`hamburger ${menuOpen ? 'open' : ''}`} onClick={handleMenuClick}>
+//           <span className="hamburger-top"></span>
+//           <span className="hamburger-middle"></span>
+//           <span className="hamburger-bottom"></span>
+//         </div>
+//         <div className='menu-item'>
+
+//           <ul id="menu" className={`z-50 display hamburger-ul ${menuOpen ? '' : 'hidden'} `}>
+//             <li>
+//               <Link to="/" className="link-hover font-bold link-bg justify-center fw-500 px-3 py-2 rounded">
+//                 Home
+//               </Link>
+//             </li>
+           
+//             <li>
+//               <Link to="/projects" className="font-bold link-bg link-hover justify-center px-3 py-2 rounded">
+//                 Projects
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to="/skills" className="font-bold link-bg link-hover justify-center px-3 py-2 rounded">
+//                 Skills
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to="/experience" className="font-bold link-bg link-hover fw-600 justify-center px-3 py-2 rounded">
+//                 Experience
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to="/contact" className="font-bold link-bg link-hover justify-center px-3 py-2 rounded">
+//                 Contact
+//               </Link>
+//             </li>
+
+//             <li>
+//             <Link to="/AboutMe" className="font-bold link-bg link-hover justify-center px-3 py-2 rounded">
+//                AboutMe
+//               </Link>
+//             </li>            
+//           </ul>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// };
+
+// export default HamburgerMenu;
+
+
+
+
+/**
+ * HamburgerMenu Component
+ *
+ * This component represents a responsive hamburger menu that can be toggled open and closed.
+ * It also includes a feature to close the menu when the user clicks outside the menu area.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+import React, { useState, useEffect, useRef, MouseEvent } from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
+import Home from './Home';
 
-const HamburgerMenu = () => {
+/**
+ * HamburgerMenu Component
+ *
+ * This component represents a responsive hamburger menu that can be toggled open and closed.
+ * It also includes a feature to close the menu when the user clicks outside the menu area.
+ */
+const HamburgerMenu: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement | null>(null);
 
   const handleMenuClick = () => {
     setMenuOpen(!menuOpen);
   };
+
+  const handleClickOutsideMenu = (event: MouseEvent<Document>) => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      setMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    // document.addEventListener('click', handleClickOutsideMenu);
+    return () => {
+      // document.removeEventListener('click', handleClickOutsideMenu);
+    };
+  },);
 
   return (
     <header id="header">
@@ -17,23 +122,14 @@ const HamburgerMenu = () => {
           <span className="hamburger-middle"></span>
           <span className="hamburger-bottom"></span>
         </div>
-        <div className='menu-item'>
-
+        <div className='menu-item' ref={menuRef}>
           <ul id="menu" className={`z-50 display hamburger-ul ${menuOpen ? '' : 'hidden'} `}>
             <li>
               <Link to="/" className="link-hover font-bold link-bg justify-center fw-500 px-3 py-2 rounded">
                 Home
               </Link>
             </li>
-            <li>
-            
-
-
-
-              {/* <Link to="/aboutMe" className="font-bold link-bg link-hover justify-center px-3 py-2 rounded">
-                About me
-              </Link> */}
-            </li>
+           
             <li>
               <Link to="/projects" className="font-bold link-bg link-hover justify-center px-3 py-2 rounded">
                 Projects
@@ -59,13 +155,8 @@ const HamburgerMenu = () => {
             <Link to="/AboutMe" className="font-bold link-bg link-hover justify-center px-3 py-2 rounded">
                AboutMe
               </Link>
-            </li>
-
-            
+            </li>            
           </ul>
-
-
-
         </div>
       </div>
     </header>
